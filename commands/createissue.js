44cea0e505551a41dc8043d1118e1bfeac346307
SmusiJarviss <84509587+SmusiJarviss@users.noreply.github.com>
2023-03-14 21:34:53 +0100

    Add files via upload

--- commands/createissue.js
+++ commands/createissue.js
@@ -1,4 +1,19 @@
-const { SlashCommandBuilder, ActionRowBuilder, Events, ModalBuilder, TextInputBuilder, TextInputStyle  } = require('discord.js');
+const { SlashCommandBuilder, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle  } = require('discord.js');
+
+class Builder
+{
+	constructor(id = String, label = String, isRequired = Boolean, value = String, style = TextInputStyle)
+	{
+		const txt = new TextInputBuilder()
+				.setCustomId(id)
+				.setLabel(label)
+				.setRequired(isRequired)
+				.setValue(value)
+				.setStyle(style);
+
+		return txt;
+	};
+};
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -9,18 +24,8 @@ module.exports = {
 				.setCustomId("sendissue")
 				.setTitle("Crea un Issue su GitHub");
 		
-		const titleInput = new TextInputBuilder()
-				.setCustomId("title")
-				.setLabel("Titolo")
-				.setRequired(true)
-				.setStyle(TextInputStyle.Short);
-
-		const descriptionInput = new TextInputBuilder()
-				.setCustomId("description")
-				.setLabel("Descrizione")
-				.setRequired(true)
-				.setValue("Descrizione accurata con tutti i dettagli.")
-				.setStyle(TextInputStyle.Paragraph);
+		const titleInput = new Builder("title", "Titolo", true, "Inserisci il titolo", TextInputStyle.Short);
+		const descriptionInput = new Builder("description", "Descrizione", true, "Descrizione accurata con tutti i dettagli.", TextInputStyle.Paragraph);
 
 		modal.addComponents(new ActionRowBuilder().addComponents(titleInput), new ActionRowBuilder().addComponents(descriptionInput));
 
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');    
+const { token, git_token } = require('./config.json');    
 const { Octokit } = require('octokit')
 
-const octokit = new Octokit({ auth: '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' })
+const octokit = new Octokit({ auth: git_token })
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
@@ -26,6 +26,14 @@ for (const file of commandFiles)
 		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 	}
 }
+
+client.once(Events.ClientReady, async () => 
+{
+    if (await (await octokit.request('GET /rate_limit', {} )).status == 200)
+        return console.log('\x1b[42m Connected successfully to the GitHub API. \x1b[0m');
+    
+    console.log('\x1b[41m No connection to the GitHub API, maybe GitHub_Token is invalid or rate limit occurred. \x1b[0m');
+});
 
 client.on(Events.InteractionCreate, async interaction => 
 {
@@ -59,18 +67,25 @@ client.on(Events.InteractionCreate, async interaction =>
     
     if (!interaction.isModalSubmit()) return;
 
-	await octokit.rest.issues.create({
-        owner: "Omega-SCP",
-        repo: "OmegaCore",
-        title: interaction.fields.getTextInputValue("title"),
-        body: interaction.fields.getTextInputValue("description") + `\nIssue creato da: ${interaction.user.username + "#" + interaction.user.discriminator}`,
-        labels: 
-        [
-            'bug'
-        ]
-    });
-
-    interaction.reply({ content: "Issue creato con successo", ephemeral: true})
+    try
+    {
+        await octokit.rest.issues.create({
+            owner: "Omega-SCP",
+            repo: "OmegaCore",
+            title: interaction.fields.getTextInputValue("title"),
+            body: interaction.fields.getTextInputValue("description") + `\nIssue creato da: ${interaction.user.username + "#" + interaction.user.discriminator}`,
+            labels: 
+            [
+                'bug'
+            ]
+        });
+    
+        interaction.reply({ content: "Issue creato con successo", ephemeral: true});
+    }
+    catch (error)
+    {
+        console.log(error);
+    }
 });
 
 client.login(token);
