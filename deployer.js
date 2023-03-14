bddbaa345fcfc267a23bf0ab9926ba6181029774
SmusiJarviss <84509587+SmusiJarviss@users.noreply.github.com>
2023-03-14 21:36:44 +0100

    Add files via upload

--- deployer.js
+++ deployer.js
@@ -1,5 +1,5 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, token } = require('./config.json');
+const { clientId, token, guildId } = require('./config.json');
 const fs = require('node:fs');
 
 const commands = [];
@@ -20,7 +20,7 @@ const rest = new REST({ version: '10' }).setToken(token);
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
 		const data = await rest.put(
-			Routes.applicationGuildCommands(clientId, "1020700935049199678"),
+			Routes.applicationGuildCommands(clientId, guildId),
 			{ body: commands },
 		);
 
